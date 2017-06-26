b7eded889a240929526d89d4e8ab7206d4f6eff7
Jordan Cooper <jordan@cooperplanet.com>
2017-06-25 22:49:07 -0600

    Show and hide team input

--- src/components/PokemonTeam/PokemonTeam.js
+++ src/components/PokemonTeam/PokemonTeam.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import './PokemonTeam.css'
 import TeamMember from '../PokemonPicker/TeamMember/TeamMember'
+import {dispatchAddToTeam} from '../../services/pokemonService'
 
 export default class PokemonTeam extends Component {
   constructor() {
@@ -8,16 +9,19 @@ export default class PokemonTeam extends Component {
 
     this.state = {
       userInput: "",
-      pokemonName: ""
+      addingPokemon: false,
+      currentSlot: null
     }
     this.handleChange = this.handleChange.bind(this)
-    this.handleSearch = this.handleSearch.bind(this)
-    this.handleRandom = this.handleRandom.bind(this)
+    this.handleAdd = this.handleAdd.bind(this)
+    this.handleSelect = this.handleSelect.bind(this)
   }
 
-  handleRandom() {
+
+  handleSelect(slot) {
     this.setState({
-      pokemonName: (Math.floor(Math.random() * 721) + 1).toString()
+      addingPokemon: true,
+      currentSlot: slot
     })
   }
 
@@ -27,33 +31,36 @@ export default class PokemonTeam extends Component {
     })
   }
 
-  handleSearch() {
+  handleAdd() {
+    dispatchAddToTeam(this.state.userInput, this.state.currentSlot)
+
     this.setState({
-      pokemonName: this.state.userInput,
-      userInput: ""
+      userInput: "",
+      addingPokemon: false,
+      currentSlot: null
     })
+
   }
 
   render() {
 
     return (
       <div>
-        
         <div className="pokemon-team">
-          <div className="team-member"><TeamMember name="bulbasaur" index={0} /></div>
-          <div className="team-member"><TeamMember name="nidoking" index={1} /></div>
-          <div className="team-member"><TeamMember name="gengar" index={2} /></div>
-          <div className="team-member"><TeamMember name="gengar" index={3} /></div>
-          <div className="team-member"><TeamMember name="gengar" index={4} /></div>
-          <div className="team-member"><TeamMember name="gengar" index={5} /></div>
+          <div className="team-member" onClick={() => this.handleSelect(0)}><TeamMember name="bulbasaur" index={0} /></div>
+          <div className="team-member" onClick={() => this.handleSelect(1)}><TeamMember name="nidoking" index={1} /></div>
+          <div className="team-member" onClick={() => this.handleSelect(2)}><TeamMember name="gengar" index={2} /></div>
+          <div className="team-member" onClick={() => this.handleSelect(3)}><TeamMember name="gengar" index={3} /></div>
+          <div className="team-member" onClick={() => this.handleSelect(4)}><TeamMember name="gengar" index={4} /></div>
+          <div className="team-member" onClick={() => this.handleSelect(5)}><TeamMember name="gengar" index={5} /></div>
         </div>
         
-          <div className="input-container">
+          <div className={`input-container ${this.state.addingPokemon ? "" : "hide-console"}`}>
             <input className="console" type="text" placeholder="Enter a pokemon..." value={this.state.userInput}
                    onChange={(e) => this.handleChange(e.target.value)}/>
-            <button className="add-button" onClick={this.handleSearch}>Add!</button>
+            <button className="add-button" onClick={this.handleAdd}>Add!</button>
           </div>
-        </div>
+      </div>
     )
   }
 }
