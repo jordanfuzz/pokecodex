e17940006efa98e6826e5afccaab26782ae968eb
Jordan Cooper <jordan@cooperplanet.com>
2023-11-04 00:32:34 -0600

    Remove old code

--- old-src/store.js
+++
@@ -1,6 +0,0 @@
-import {createStore, applyMiddleware} from 'redux'
-import promiseMiddleware from 'redux-promise-middleware'
-
-import reducer from './ducks/reducer'
-
-export default createStore(reducer, applyMiddleware(promiseMiddleware()))
